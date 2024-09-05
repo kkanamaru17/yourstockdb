669f3743f94781ec1a8f8bd7257eb45a75bcddb9
kkanamaru17 <98554406+kkanamaru17@users.noreply.github.com>
2024-09-05 21:02:51 +0900

    session lifetime set

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+from datetime import timedelta
+
 
 class Config:
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Set the session lifetime to, for example, 5 days
+    PERMANENT_SESSION_LIFETIME = timedelta(days=5)
+    # Optionally, you can control the "remember me" cookie lifetime
+    REMEMBER_COOKIE_DURATION = timedelta(days=5)
